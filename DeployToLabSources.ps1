c66a070935c398ca1c36947a578b751fa9e367d1
jpbruckler <jpbruckler@gmail.com>
2024-12-30 00:18:29 -0500

    feat: add script to deploy to lab sources

---
+++ DeployToLabSources.ps1
@@ -0,0 +1,21 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [string] $LabSourcesPath
+)
+
+if (-not (Test-Path $LabSourcesPath))
+{
+    Write-Error "LabSourcesPath does not exist: $LabSourcesPath"
+    exit 1
+}
+$InformationPreference = 'Continue'
+
+Write-Information "Copying Custom Roles to $LabSourcesPath"
+Copy-Item -Path (Join-Path $PSScriptRoot 'CustomRoles') -Destination $LabSourcesPath -Recurse -Force
+
+Write-Information "Copying Lab Definitions to $LabSourcesPath"
+Copy-Item -Path (Join-Path $PSScriptRoot 'LabDefinitions') -Destination $LabSourcesPath -Recurse -Force
+
+Write-Information "Copying PostInstallationActivities to $LabSourcesPath"
+Copy-Item -Path (Join-Path $PSScriptRoot 'PostInstallationActivities') -Destination $LabSourcesPath -Recurse -Force
